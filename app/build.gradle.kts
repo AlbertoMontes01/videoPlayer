1f5ff30884c0869b54012f97273e91ec404c5db8
Alberto <dav.vazquez1719@gmail.com>
2026-01-02 16:15:03 -0600

    dependecyVersionAdded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,73 +1,60 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     kotlin("kapt")
 }
 
-
 android {
     namespace = "com.example.myapplication"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
-    // Core / Compose
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
 
-    // Media3 (ExoPlayer)
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Media3
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
 
-    // Navigation Compose
-    implementation(libs.androidx.navigation.compose)
-
-    // 🔥 Hilt (OBLIGATORIO)
+    // Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
-
-    // 🔥 Hilt + Navigation Compose
     implementation(libs.androidx.hilt.navigation.compose)
-
-    // Networking
-    implementation(libs.retrofit.v2110)
-    implementation(libs.converter.gson.v2110)
 }
